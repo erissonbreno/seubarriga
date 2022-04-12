baad5475293653d8942b2e0cc89cd554793bd4d8
erissonbreno <erissonbreno@hotmail.com>
2022-04-11 22:04:30 -0300

    Primeiro commit

---
+++ test/jest.test.js
@@ -0,0 +1,13 @@
+test('Devo conhecer as principais assertivas do jest', () => {
+  let number = null;
+  expect(number).toBeNull();
+  number = 10;
+  expect(number).not.toBeNull();
+  expect(number).toEqual(10);
+  expect(number).toBeGreaterThan(9);
+});
+
+test('Devo saber trabalhar com objetos', () => {
+  const obj = { name: 'John', mail: 'john@mail.com' };
+  expect(obj).toHaveProperty('name', 'John');
+})
